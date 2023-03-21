5391ab509c0740dfcb03d7fa643b0a7bc62ca190
liquanxiao <1614001993@qq.com>
2023-03-21 16:28:17 +0800

    封装

---
+++ mOop/src/com/itheima/privteDemo/PriviteDemoTest.java
@@ -0,0 +1,16 @@
+package com.itheima.privteDemo;
+
+public class PriviteDemoTest {
+    public static void main(String[] args) {
+        PrivteDemo pr = new PrivteDemo();
+        pr.brand="大众";
+        pr.setName("v8发动机");;
+        //被私有化
+        // pr.price=666.00;
+        pr.setPrice(899);
+        System.out.println(pr.getPrice());
+        System.out.println(pr.getName());
+        pr.info();
+
+    }
+}
---
+++ mOop/src/com/itheima/privteDemo/PrivteDemo.java
@@ -0,0 +1,33 @@
+package com.itheima.privteDemo;
+
+public class PrivteDemo {
+    String brand;
+    private double price;
+    private String name;
+
+
+    //修改获取价格
+    public void setPrice(double price) {
+        if (price <= 0) {
+            System.out.println("您输入的价格有误");
+
+        } else {
+            System.out.println("价格合理，修改成功");
+            this.price = price;
+        }
+    }
+    public double getPrice(){
+        return price;
+    }
+
+    //修改获取发动机名称
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return this.name=name;
+    }
+    public void info(){
+        System.out.println(brand+"的发动机为"+name+",价格为"+price);
+    }
+}
