ac9e48723d44b95ad2965fcd5de0d36925bc8245
liquanxiao <1614001993@qq.com>
2023-03-21 18:23:43 +0800

    实体类案例以及电影查询案例

---
+++ mOop/src/com/itheima/moviedemo/Movie.java
@@ -0,0 +1,84 @@
+package com.itheima.moviedemo;
+
+public class Movie {
+    private int id;
+    private String name;
+    private double price;
+    private  double score;
+
+    public Movie() {
+    }
+
+
+    public Movie(int id, String name, double price, double score) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.score = score;
+    }
+
+    /**
+     * 获取
+     * @return id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * 设置
+     * @param id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 获取
+     * @return price
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    /**
+     * 设置
+     * @param price
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+    /**
+     * 获取
+     * @return score
+     */
+    public double getScore() {
+        return score;
+    }
+
+    /**
+     * 设置
+     * @param score
+     */
+    public void setScore(double score) {
+        this.score = score;
+    }
+}
